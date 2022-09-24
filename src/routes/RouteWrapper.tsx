ac9cd76be54d0acf5c43c712e438e49614799097
Kelvin Hong <kelvinok52@gmail.com>
2022-09-24 21:04:09 +0700

    tmp expoauth

--- src/routes/RouteWrapper.tsx
+++ src/routes/RouteWrapper.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
 import Button from '../components/Button';
 import Layout from '../components/Layout';
 
 const RouteWrapper = () => {
-  const handleAuth = () => {};
+  const handleAuth = async () => {
+    console.log('click');
+    try {
+      // const hasHardware = await LocalAuthentication.hasHardwareAsync();
+      // console.log(hasHardware);
+      const securityLevel = await LocalAuthentication.getEnrolledLevelAsync();
+      console.log(securityLevel);
+      const biometricAuth = await LocalAuthentication.authenticateAsync({
+        promptMessage: 'Login with Biometrics',
+        disableDeviceFallback: false,
+        cancelLabel: 'Cancel',
+      });
+
+      console.log(biometricAuth);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Layout>
@@ -22,6 +40,7 @@ const styles = StyleSheet.create({
   title: {
     marginBottom: 20,
     fontSize: 18,
+    color: '#000',
   },
   container: {
     flex: 1,
